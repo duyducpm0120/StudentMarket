0e92db0a3923664bcdf0b9e6535c29c31dbc11ad
Phan Duy Đức <ducduypm0120@gmail.com>
2022-06-16 13:01:19 +0700

    Update EndpointConstant.java

--- app/src/main/java/com/example/studentmarket/Constants/EndpointConstant.java
+++ app/src/main/java/com/example/studentmarket/Constants/EndpointConstant.java
@@ -19,4 +19,5 @@ public class EndpointConstant {
     public static final String UNSAVE_PRODUCT_FAVORITE = ENDPOINT_PREFIX + "listings/unsave";
     public static final String UPDATE_USER_PROFILE = ENDPOINT_PREFIX + "user/update";
     public static final String UPDATE_USER_AVATAR = ENDPOINT_PREFIX + "user/updateProfilePic";
+    public static final String GET_MY_LIST_PRODUCT = ENDPOINT_PREFIX + "listings/myListings";
 }
